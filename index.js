c52e5155bda424d531ac348b1365ed8a58bf04aa
treyjaded <trevonjcharles@gmail.com>
2022-09-29 19:14:57 +0000

    Completed printTime function

--- index.js
+++ index.js
@@ -66,3 +66,13 @@ function largest(num1,num2,num3){
     return Math.max(num1, num2, num3)
     
 }console.log(largest(4,62,8))
+
+function printTime (){
+
+var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    return today
+}
+
+console.log(printTime())
