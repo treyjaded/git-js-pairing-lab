7f19725dab67b0ecf2b61a73d080f1a3d5ee2bea
treyjaded <trevonjcharles@gmail.com>
2022-09-29 18:26:37 +0000

    Completed multiplesOfThreeOrFive function

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function multiplesOfThree() {
     }
 }
 multiplesOfThree()
+
+function multiplesOfThreeOrFive() {
+    for (let i = 3; i <=100; i++)
+     {if (i % 3 === 0 || i % 5 === 0) {
+         console.log(i)
+         
+             }
+        }
+    }
+    
+multiplesOfThreeOrFive()
