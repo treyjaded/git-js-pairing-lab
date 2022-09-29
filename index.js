f71e0ff9abbbd7b5d9d1834c6b1f0c16a7dcee77
treyjaded <trevonjcharles@gmail.com>
2022-09-29 18:46:51 +0000

    Completed isNegative function

--- index.js
+++ index.js
@@ -43,3 +43,12 @@ function add(num1,num2){
     }
 }
 console.log(add(2,2))
+
+function isNegative(num){
+    if (num < 0){
+        return true
+    }
+     else return false 
+}
+console.log(isNegative (Math.PI))
+
